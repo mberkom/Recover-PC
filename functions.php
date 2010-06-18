1d4acc7648952bcd242910d9d34292b652e7688e
Michael Berkompas <mberkom@gmail.com>
2010-06-18 11:45:45 -0700

    Initial files

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+
+	/**
+	 * Recover PC Base Functions
+	 * 
+	 * PHP version 5
+	 *
+	 * LICENSE: Contained in the root folder under the file license.txt
+	 * 
+	 * @author	Michael Berkompas, mberkom@gmail.com
+	 * @copyright 2010 Michael Berkompas
+	 */
+
+?>
---
+++ recover.php
@@ -0,0 +1,22 @@
+<?php
+
+	/**
+	 * Recover PC Action Handler
+	 * 
+	 * PHP version 5
+	 *
+	 * LICENSE: Contained in the root folder under the file license.txt
+	 * 
+	 * @author	Michael Berkompas, mberkom@gmail.com
+	 * @copyright 2010 Michael Berkompas
+	 */
+
+
+	/* Definitions */
+	
+	$db['username'] = "USERNAME HERE";
+	$db['password'] = "PASSWORD HERE";
+	$db['database'] = "DATABASE HERE";
+	$db['table'] = "TABLENAME HERE";
+	
+?>
